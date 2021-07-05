1ecca596a5c32dd28bc6f421d9b212b27968a9f0
lsh <lsh910703@gmail.com>
2021-07-05 19:17:06 +0900

    issues are resolved

--- SC_loss.py
+++ SC_loss.py
@@ -3,9 +3,9 @@ import jax.numpy as np
 from jax import jit, random
 
 
-
 from transformers import FlaxCLIPModel
 
+batch_size = 256
 N_samples = 128
 test_inner_steps = 64
 
@@ -39,17 +39,19 @@ def pose_spherical(theta, phi, radius):
     c2w = np.array([[-1,0,0,0],[0,0,1,0],[0,1,0,0],[0,0,0,1]]) @ c2w
     return c2w
 
-def random_poses(rng, bds):
+def get_rays(H, W, focal, c2w):
+    i, j = np.meshgrid(np.arange(W, step = step), np.arange(H, step = step), indexing='xy')
+    dirs = np.stack([(i-W*.5)/focal, -(j-H*.5)/focal, -np.ones_like(i)], -1)
+    rays_d = np.sum(dirs[..., np.newaxis, :] * c2w[:3,:3], -1)
+    rays_o = np.broadcast_to(
+        c2w[:3,-1], rays_d.shape)
+    return np.stack([rays_o, rays_d], 0)
+
+def random_pose(rng, bds):
     radius = random.uniform(rng, minval = bds[0], maxval = bds[1])
     theta = random.uniform(rng, minval = 0, maxval = 2*np.pi)
     phi = random.uniform(rng, minval = 0, maxval = np.pi/2)
-    return np.stack([pose_spherical(r,t,p) for r, t, p in zip(radius, theta, phi)])
-
-# def random_poses(rng, bds):
-#     radius = random.uniform(rng, shape = [batch_size], minval = bds[0], maxval = bds[1])
-#     theta = random.uniform(rng, shape = [batch_size], minval = 0, maxval = 2*np.pi)
-#     phi = random.uniform(rng, shape = [batch_size], minval = 0, maxval = np.pi/2)
-#     return np.stack([pose_spherical(r,t,p) for r, t, p in zip(radius, theta, phi)])
+    return pose_spherical(radius,theta,phi)
 
 def CLIPProcessor(image):
     '''
@@ -63,66 +65,76 @@ def CLIPProcessor(image):
     image = (image - np.array([0.48145466, 0.4578275, 0.40821073]).reshape(1,3,1,1)) / np.array([0.26862954, 0.26130258, 0.27577711]).reshape(1,3,1,1)
     return image
 
-@jit
-def SC_loss(target_emb, source_img, l):
+def SC_loss(rng_inputs, model, params, target_img, target_emb, rays, l):
     '''
         target_emb [B, D]: pre-computed target embedding vector \phi(I)
         source_img [B, 3, H, W]: source image \hat{I}
         l: loss weight lambda
         return: SC_loss
     '''
+    source_img = np.clip(render_rays(rng_inputs[1], model, params, rays, N_samples, rand=False), 0, 1)
+    source_img = np.reshape(source_img, [1,64,64,3]).transpose(0,3,1,2)
     source_img = CLIPProcessor(source_img)
     source_emb = CLIP_model.get_image_features(pixel_values=source_img)
     source_emb /= np.linalg.norm(source_emb, axis=-1, keepdims=True)
-    return l * (target_emb * source_emb).sum()/source_emb.shape[0]
+    return l/2 * ((target_emb - source_emb)**2).sum()/source_emb.shape[0]
 
+# update inner model network weights with once step of sgd
 @jit
-def single_step(rng, image, rays, params, bds, step, target_emb):
-    '''
-        step [] : current step
-        target_emb [B, D]: pre-computed target embedding vector \phi(I)
-
-        TODO:
-        implement random pose
-    '''
+def single_step(rng, images, rays, params, target_emb, step):
     def sgd(param, update):
         return param - inner_step_size * update
 
-    rng, rng_inputs = jax.random.split(rng)
+    def loss_fn(params, rng_inputs):
+        pixels = np.reshape(images, [-1,3])
+        pixel_rays = np.reshape(rays, (2,-1,3))
+        idx = random.randint(rng_inputs[0], shape=(batch_size,), minval=0, maxval=pixels.shape[0])
+        pixel_sub = pixels[idx,:]
+        pixel_ray_sub = pixel_rays[:,idx,:]
 
-    def loss_model(params):
-        g = render_rays(rng_inputs, model, params, None, rays, bds[0], bds[1], N_samples, rand=True)
-        L = mse_fn(g, image)
+        idx = random.randint(rng_inputs[0], shape=(1,), minval=0, maxval=images.shape[0])
+        image_sub = images[idx]
+        target_emb_sub = target_emb[idx,:] 
+        ray_sub = np.reshape(rays[:,idx][:,:,::2,::2], [2,-1,3])
 
-        if step%K == 0:
-            # these lines are not confirmed
-            rays = get_ray(hwf[0], hwf[1], hwf[2], random_pose(rng_inputs))
-            rays = np.reshape(rays, (2,-1,3))
-            source_img = np.clip(render_rays(rng_inputs, model, params, None, rays, bds[0], bds[1], N_samples, rand=False), 0, 1)
-            L += SC_loss(target_emb, source_img, l) # add Semantic Consistency loss
+        
+        g = render_rays(rng_inputs[1], model, params, pixel_ray_sub, N_samples, rand=True)
+        L = mse_fn(g, pixel_sub)
+
+        L = jax.lax.cond(step%K == 0,
+            lambda _: L + SC_loss(rng_inputs, model, params, image_sub, target_emb_sub, ray_sub, 1),
+            lambda _: L, 
+            operand=None
+        )
         return L
 
-    model_loss, grad = jax.value_and_grad(loss_model)(params)
-    new_params = jax.tree_multimap(sgd, params, grad)
-    return rng, new_params, model_loss
+    rng, *rng_inputs = jax.random.split(rng, 3)
+    loss, grad = jax.value_and_grad(loss_fn)(params, rng_inputs)
+    params = jax.tree_multimap(sgd, params, grad)
+    return rng, params, loss
+
+# update inner model network weights inner_update_steps number of times
+def update_network_weights(rng, images, rays, params, target_emb, step):
+    for _ in range(inner_update_steps):
+        rng, params, loss = single_step(rng, images, rays, params, target_emb, step)
+    return rng, params, loss
+
+# update meta model weights based on trained inner model weights
+def update_model(rng, params, images, rays, target_emb, step):
+    rng, new_params, model_loss = update_network_weights(rng, images, rays, params, target_emb, step)
 
 '''
-# libraries for checking SC_loss
-from PIL import Image
-import requests
+rng = jax.random.PRNGKey(0)
+num_test = 5
+rng, rng_input = jax.random.split(rng)
+train_images, test_images = np.split(images, [images.shape[0]-num_test], axis=0)
+train_poses, test_poses = np.split(poses, [images.shape[0]-num_test], axis=0)
 
-# in our project target_emb'll be pre-computed and fed by dataloader
-target_url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-target_img = np.array(Image.open(requests.get(target_url, stream=True).raw))
-target_img = np.expand_dims(target_img.transpose(2,0,1), 0)/255
-target_img = CLIPProcessor(target_img)
-target_emb = model.get_image_features(pixel_values=target_img)
+target_images = CLIPProcessor(train_images.transpose(0,3,1,2))
+target_emb = CLIP_model.get_image_features(pixel_values=target_images)
 target_emb /= np.linalg.norm(target_emb, axis=-1, keepdims=True)
 
-# in our project source image'll come from NeRF model's prediction
-source_img_url = "https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg"
-source_img = np.array(Image.open(requests.get(source_img_url, stream=True).raw))/255
-source_img = np.expand_dims(source_img.transpose(2,1,0), 0)
-
-print(SC_loss(target_emb, source_img, 1))
+for step in tqdm(range(2)):
+    rays = get_ray_batch(hwf[0], hwf[1], hwf[2], train_poses)
+    update_model(rng, params, train_images, rays, target_emb, step)
 '''
